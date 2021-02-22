c988cfe56e4a2b5a6558ab062151c04394e1b71d
Rachel Gitovich <rachelibr050@gmail.com>
2021-02-22 22:51:07 +0200

    create Board component

---
+++ src/pieces/chessPieces.js
@@ -0,0 +1,32 @@
+const chessPieces = {
+	white: {
+		king: '9812;',
+		queen: '9813;',
+		rook: '9814;',
+		bishop: '9815;',
+		knight: '9816;',
+		pawn: '9817;'
+	},
+	black: {
+		king: '9818;',
+		queen: '9819;',
+		rook: '9820;',
+		bishop: '9821;',
+		knight: '9822;',
+		pawn: '9823;'
+	}
+};
+
+export const wkg = chessPieces.white.king;
+export const wq = chessPieces.white.queen;
+export const wr = chessPieces.white.rook;
+export const wb = chessPieces.white.bishop;
+export const wkt = chessPieces.white.knight;
+export const wp = chessPieces.white.pawn;
+
+export const bkg = chessPieces.black.king;
+export const bq = chessPieces.black.queen;
+export const br = chessPieces.black.rook;
+export const bb = chessPieces.black.bishop;
+export const bkt = chessPieces.black.knight;
+export const bp = chessPieces.black.pawn;
